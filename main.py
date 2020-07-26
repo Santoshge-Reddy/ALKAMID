373a97757d0e79501a07be886ff333bccf6260e9
santoshge <g.santosh.sunny@gmail.com>
2020-07-26 22:36:46 +0530

    'commits'

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ alkamid = Alkamid()
 # dictionaries for chemical_details and chemicals_in_plants 
 chemical_details = alkamid.chemical_details
 chemicals_in_plants = alkamid.chemicals_in_plants
+print('====================Start======================')
+print('dictionaries for chemical_details')
+print(chemical_details)
+print('=====================End=================')
+
+print('====================Start======================')
+print('dictionaries for chemicals_in_plants')
+print(chemicals_in_plants)
+print('=====================End=================')
 
 
 
@@ -14,9 +23,18 @@ chemicals_in_plants = alkamid.chemicals_in_plants
 # df_chemical_details = alkamid.df_chemical_details
 # df_chemicals_in_plants = alkamid.df_chemicals_in_plants
 chemical_dataframe = alkamid.chemical_dataframe
+print('====================Start======================')
+print('dataframe for chemical details')
+print(chemical_dataframe)
+print('=====================End=================')
+
 
 # # statistics for the  data
 statistics = alkamid.statistics
+print('====================Start======================')
+print('statics for the data')
+print(statistics)
+print('=====================End=================')
 
 
 # run this command to save data to json files
